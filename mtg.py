b27015062f25b1e476d0bf7186367981531c9299
Matthew Holden <matthew.holden86@gmail.com>
2023-09-18 16:30:18 -0400

    can handle any n-gram level

--- mtg.py
+++ mtg.py
@@ -1,81 +1,30 @@
 import re
 
-def define_ngram(corpus: list):
-    trigram_dict = {}
-    bigram_dict = {}
-    unigram_dict = {}
+def define_vocab(corpus):
     vocab = []
-
-    for i in range(len(corpus) - 2):
-        trigram = (corpus[i], corpus[i+1], corpus[i+2])
-        if trigram in trigram_dict.keys():
-            #trigram_dict[trigram]['count'] += 1
-            trigram_dict[trigram] += 1
-        else:
-            #trigram_dict[trigram] = {}
-            #trigram_dict[trigram]['count'] = 1
-            #trigram_dict[trigram]['index'] = i
-            trigram_dict[trigram] = 1
-
-
-    for i in range(len(corpus) - 1):
-        bigram = (corpus[i], corpus[i+1])
-        if bigram in bigram_dict.keys():
-            #bigram_dict[bigram]['count'] += 1
-            bigram_dict[bigram] += 1
-        else:
-            #bigram_dict[bigram] = {}
-            #bigram_dict[bigram]['count'] = 1
-            #bigram_dict[bigram]['index'] = i
-            bigram_dict[bigram] = 1
-    
     for i in range(len(corpus)):
-        unigram = corpus[i]
-        if unigram in unigram_dict.keys():
-            #unigram_dict[unigram]['count'] += 1
-            unigram_dict[unigram] += 1
+        word = corpus[i]
+        if word in vocab:
+            continue
         else:
-            #unigram_dict[unigram] = {}
-            #unigram_dict[unigram]['count'] = 1
-            #unigram_dict[unigram]['index'] = i
-            unigram_dict[unigram] = 1
-            vocab.append(unigram)
+            vocab.append(word)
+    return vocab    
 
-    #print(unigram_dict)
-
-    return trigram_dict, bigram_dict, unigram_dict, vocab
-
+def train_model(corpus:list, n:int):
+    n_gram_dict = {}
+    for i in range(len(corpus) - (n-1)):
+        gram = ",".join(corpus[i:i+n])
+        if gram in n_gram_dict.keys():
+            n_gram_dict[gram] += 1
+        else:
+            n_gram_dict[gram] = 1
+    return n_gram_dict
 
 def finish_sentence(sentence: list, n: int, corpus: list, randomize=False):
-    trigram_dict, bigram_dict, unigram_dict, vocab = define_ngram(corpus)
+    vocab = define_vocab(corpus)
     corpus_length = len(corpus)
-    current_history = {
-        '3': {'input' : tuple(sentence[-2:]), 'testgramdict' : trigram_dict, 'lowergramdict' : bigram_dict},
-        '2': {'input' : str(sentence[-1]), 'testgramdict' : bigram_dict, 'lowergramdict' : unigram_dict},
-        '1': {'testgramdict': unigram_dict}
-    }
-
-    potential_word = find_sequence(current_history, n, vocab, corpus_length, randomize)
-    if len(potential_word) > 1:
-        current_best_index = None
-        for word in potential_word:
-            sequence = list(current_history[str(n)]['input'])+[word]
-            #print(sequence)
-            for i in range(len(corpus) - (n-1)):
-                test_seq = corpus[i:i+n]
-                if (test_seq == sequence) & (current_best_index == None):
-                    current_best_index = i
-                    next_word = word
-                elif test_seq == sequence:
-                    if i < current_best_index:
-                        current_best_index = i
-                        next_word = word 
-                    else:
-                        continue
-        sentence.append(next_word)
-    else:
-        next_word = potential_word[0]
-        sentence.append(next_word)
+    next_word = find_sequence(sentence, n, vocab, corpus_length, corpus, randomize)
+    sentence.append(next_word)
     print(f"The next best word is '{next_word}'")
     if (len(sentence)==10) | (sentence[-1]== "."):
         print(sentence)
@@ -83,40 +32,34 @@ def finish_sentence(sentence: list, n: int, corpus: list, randomize=False):
     else:
         return finish_sentence(sentence=sentence, n=n, corpus=corpus, randomize=False)
                     
-#
-def find_sequence(current_history: dict, n:int, vocab, corpus_length, randomize):
-    while n > 0:#
-        #print(n)
-        if n == 0:
-            return "Fuck why is n 0"
+def find_sequence(sentence:list, n:int, vocab, corpus_length, corpus, randomize):
+    while n > 0:
         word_scores = {}
         if n > 1:
-            lower_gram = current_history[str(n)]['input']
-            #print(lower_gram)
-            #print(current_history[str(n)]['lowergramdict'].get(lower_gram, 0))
-            if current_history[str(n)]['lowergramdict'].get(lower_gram, 0) == 0:
+            input_text = ",".join(sentence[-(n-1):])
+            testgramdict = train_model(corpus, n)
+            lowergramdict = train_model(corpus, n-1)
+            lower_gram = input_text
+            if lowergramdict.get(lower_gram, 0) == 0:
                 print("Need Lower N-Gram Up Here")
                 n-=1
                 continue
+        elif n == 1:
+            input_text = ""
+            testgramdict = train_model(corpus, n)
 
         for word in vocab:
-            #print(type(word))
             if n > 1:
-                #print(f'evaluating word: {word}')
-                if type(current_history[str(n)]['input']) == tuple:
-                    test_gram = tuple(list(current_history[str(n)]['input']) + [word])
-                elif type(current_history[str(n)]['input']) == str:
-                    test_gram = tuple([current_history[str(n)]['input'], word])
-                #print(f'TEST_GRAM {test_gram}')
-                lower_gram = current_history[str(n)]['input']
-                test_gram_count = current_history[str(n)]['testgramdict'].get(test_gram, 0)
-                lower_gram_count = current_history[str(n)]['lowergramdict'].get(lower_gram, 0)
+                if type(lower_gram) == tuple:
+                    pass
+                elif type(lower_gram) == str:
+                    test_gram = lower_gram + "," + word
+                test_gram_count = testgramdict.get(test_gram, 0)
+                lower_gram_count = lowergramdict.get(lower_gram, 0)
                 word_scores[word] = test_gram_count / lower_gram_count
             elif n == 1:
-                word_scores[word] = current_history[str(n)]['testgramdict'][word] / corpus_length
+                word_scores[word] = testgramdict[word] / corpus_length
         
-        #print(word_scores)
-        #input()
         if any(value > 0 for value in word_scores.values()):
             print('next word has been identified')
             highest_score = max(word_scores.values())
@@ -124,12 +67,34 @@ def find_sequence(current_history: dict, n:int, vocab, corpus_length, randomize)
             for key, value in word_scores.items():
                 if value == highest_score:
                     best_words.append(key)
-            return best_words
-
+            #return best_words
         else:
             n-=1
             print("Need Lower N-Gram Down Here")
-
+        if len(best_words) > 1:
+            next_word = best_word_tie(best_words, input_text, corpus, n)
+        elif len(best_words) == 1:
+            next_word = best_words[0]
+        return next_word
+    
+def best_word_tie(best_words, input_text, corpus, n):
+    current_best_index = None
+    for word in best_words:
+        sequence = input_text + word
+        for i in range(len(corpus) - (n-1)):
+            test_seq = ",".join(corpus[i:i+n])
+            if (test_seq == sequence):
+                if current_best_index == None:
+                    current_best_index = i
+                    next_word = word
+                elif i < current_best_index:
+                    current_best_index = i
+                    next_word = word 
+                else:
+                    continue
+            else:
+                continue
+    return next_word
 
 if __name__ == "__main__":
     corpus = "Babe. Theres something tragic about you. Something so magic about you, dont you agree? Babe. Theres something lonesome about you. Something so wholesome about you, get closer to me."
@@ -141,5 +106,12 @@ if __name__ == "__main__":
     split_tokens = [token for token in split_tokens if token]
     lower_corpus = [w.lower() for w in split_tokens]
     #define_ngram(lower_corpus)
-    sentence = finish_sentence(["babe", "theres", "agree"], 3, lower_corpus, False)
+    sentence = finish_sentence(["robot"], 10, lower_corpus, False)
     print(sentence)
+    #vocab = define_vocab(lower_corpus)
+    #print(vocab)
+
+
+
+### determine most recent word inside the find_sequence function so that you can use the
+### n value used in find_sequence, not just the default n value passed into the function.
